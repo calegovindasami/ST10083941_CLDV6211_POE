0bdf99e36ac0d969eae1b790e208d05c10692ba8
CG-PC <cgov0406@gmail.com>
2022-07-07 16:17:45 +0200

    Added more info page.

---
+++ Pages/MoreInfo.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ST10083941_CLDV6211_POE.Pages
+{
+    public class MoreInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
